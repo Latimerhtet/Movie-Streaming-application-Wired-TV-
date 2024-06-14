ab77dcdbf450037392ee78ec73bcd867d7e94218
Kaung Htet San <latimerhtet@gmail.com>
2024-06-14 15:31:40 +0630

    making adjustment

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
   return (
     <nav
       className={
-        "flex justify-between p-5 sm:flex-col sm:text-white sm:gap-2 sm:text-center  items-center  bg-[#003049] text-white fixed top-0 left-0 right-0 z-40"
+        "flex justify-between p-5 sm:flex-col sm:text-white sm:gap-5 sm:text-center items-center  bg-[#003049] text-white fixed top-0 left-0 right-0 z-40"
       }
     >
       <div className="sm:flex sm:w-full sm:justify-between">
@@ -27,21 +27,31 @@ const Navbar = () => {
           </p>
         </Link>
         {showMenu ? (
-          <RiCloseLargeFill onClick={handleClose} />
+          <RiCloseLargeFill
+            className="text-xl cursor-pointer"
+            onClick={handleClose}
+          />
         ) : (
-          <GrMenu className="text-xl md:hidden" onClick={handleShow} />
+          <GrMenu
+            className="cursor-pointer text-xl md:hidden"
+            onClick={handleShow}
+          />
         )}
       </div>
 
       {showMenu && (
-        <div className={"flex gap-5 flex-col lg:invisible lg:hidden "}>
-          <NavLink to={"/"}>Home</NavLink>
+        <div
+          className={"navOptions flex gap-5 flex-col lg:invisible lg:hidden"}
+        >
+          <NavLink className={"active:text-red-800"} to={"/"}>
+            Home
+          </NavLink>
           <NavLink to={"/popular"}>Popular</NavLink>
           <NavLink to={"/upcoming"}>Upcoming</NavLink>
           <SearchForm show={"visible"} />
         </div>
       )}
-      <div className={"flex lg:gap-5 sm:hidden sm:invisible "}>
+      <div className={"navOptions flex lg:gap-5 sm:hidden sm:invisible "}>
         <NavLink to={"/"}>Home</NavLink>
         <NavLink to={"/popular"}>Popular</NavLink>
         <NavLink to={"/upcoming"}>Upcoming</NavLink>
